adb1cd887ce1e57c994534daf43199f58a7409d4
woblight <woblight@gmail.com>
2019-12-01 22:02:17 +0100

    update mingw link for CI

--- CI.mk
+++ CI.mk
@@ -64,7 +64,7 @@ Qt: $(qt-modules) Qt/5.11.1/mingw53_32/bin/qt.conf
 	sed -i.bak 's/Enterprise/OpenSource/' $(pwd)/Qt/5.11.1/mingw53_32/mkspecs/qconfig.pri
 
 mingw.7z:
-	wget https://datapacket.dl.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-posix/dwarf/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z -O mingw.7z
+	wget https://netix.dl.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-posix/dwarf/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z -O mingw.7z
 	
 mingw32/bin/gcc.exe: mingw.7z
 	7zr x mingw.7z
